5bbdd15e18caaaa19b8d9bcb4e7bb392a674b3a2
Ana <ana_leticia@geb.inatel.br>
2023-07-05 11:24:57 -0300

    inicialização do portfolio

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Portfólio em construção!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/analeticiabacha?tab=repositories"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Acesse meus projetos do Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
