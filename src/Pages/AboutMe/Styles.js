ea284d14bb933c99478a70e71ceb0cda18dc1eec
Carine <carinedcb@hotmail.com>
2023-08-30 19:03:45 -0300

    Corrige responsividade, e adiciona variáveis de devices
    Co-authored-by: Ana Leticia Bacha Prince <analeticiabacha@users.noreply.github.com>

--- src/Pages/AboutMe/Styles.js
+++ src/Pages/AboutMe/Styles.js
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 
+import { devices } from "../../style/device";
+
 export const Container = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  margin-bottom: 50px;
-  min-height: 100vh;
+  min-height: 100%;
+  margin-bottom: 0;
+
+  @media${devices.tablet}{
+    min-height: 77vh;
+  }
 `;
 
 export const DivAboutPurple = styled.div`
@@ -14,14 +20,23 @@ export const DivAboutPurple = styled.div`
   justify-content: center;
   flex-direction: column;
   background: #7140db;
-  width: 100%;
-  min-height: 300px;
   padding: 20px;
   gap: 20px;
+  padding-top: 2rem;
+
+  @media${devices.tablet}{
+    width: 100%;
+    min-height: 300px;
+  }
 `;
 
 export const DivAboutText = styled.div`
   padding: 20px;
+  p{
+    @media${devices.tablet}{
+     text-transform: uppercase;
+    }
+  }
 `;
 
 export const DivTitle = styled.div`
@@ -32,30 +47,40 @@ export const DivTitle = styled.div`
 export const H1 = styled.p`
   color: ${(props) => props.color};
   font-weight: bold;
-  margin-right: 5px;
-  font-size: 25px;
+  font-size: 20px;
+  margin-right: 2px;
+
+
+  @media${devices.tablet}{
+    margin-right: 5px;
+    font-size: 25px;
+  }
 `;
 
 export const H3 = styled.p`
   color: ${(props) => props.color};
   font-weight: ${(props) => (props.bold ? "bold" : "default")};
   font-size: ${(props) => props.fontSize || "18px"};
-  text-align: justify;
+  text-align: left;
   line-height: 1.1;
   margin-top: 10px;
+  
+  @media${devices.tablet}{
+    text-align: justify;
+  }
 
   &.default {
     margin-top: 0px;
   }
-
 
 `;
 
 export const DivInfos = styled.div`
   display: flex;
   background-color: #ece4fd;
-  width: 100%;
-
+  @media${devices.tablet}{
+    width: 100%;
+  }
 `;
 
 export const ContainerLeft = styled.div`
@@ -64,18 +89,31 @@ export const ContainerLeft = styled.div`
   width: 60%;
   flex-direction: column;
   gap: 20px;
-  height: 50px;
   justify-content: space-between;
+
+  @media${devices.tablet}{
+    height: 50px;
+    padding-left: 12rem;
+  }
 
 `;
 
 export const ContainerRight = styled.div`
-  width: 40%;
-  margin-top: 20px;
+  margin-top: 50px;
+  
+  @media${devices.tablet}{
+    height: 20px;
+    display: flex;
+    justify-content: center;
+  }
 `;
 
 export const DivImg = styled.div`
   width: 90%;
+
+  @media${devices.tablet}{
+    width: 50%;
+  }
 `;
 
 export const ProfileImageTwo = styled.img`
@@ -96,4 +134,14 @@ export const DivHat = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  gap: 0.6rem;
+
 `;
+
+export const DivPortifolio = styled.div`
+  p{
+    margin: 2rem;
+    text-align: center;
+  }
+ 
+`
--- src/Pages/Projects/Styles.js
+++ src/Pages/Projects/Styles.js
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 
+import { devices } from "../../style/device";
+
 export const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center; /* Alinha as divs horizontalmente */
   align-items: center; /* Alinha as divs verticalmente */
   min-height: 100vh;
-  gap: 20px;
-  margin: 150px 0;
+  gap:2rem;
+  margin:1rem;
+
+  @media${devices.tablet}{
+    gap: 20px;
+  }
 `;
